b75896129a63b84f52a523c7bb52e0b0cc671e22
Candra <candraherdiansyah14@gmail.com>
2022-11-05 21:47:34 +0700

    membuat hello world

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Tugas Mobile",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Tugas Mobile"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    );
+  }
+}
