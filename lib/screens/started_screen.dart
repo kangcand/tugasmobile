9d7d8ee8b05d1a43c05b076f960de1173a9545ff
candra <candraherdiansyah14@gmail.com>
2022-11-07 13:53:19 +0700

    icon & background

--- lib/screens/started_screen.dart
+++ lib/screens/started_screen.dart
@@ -4,12 +4,23 @@ class StartedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Tugas Mobile"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text("Hello World"),
+      // appBar: AppBar(
+      //   title: Text("Tugas Mobile"),
+      //   centerTitle: true,
+      // ),
+      body: SafeArea(
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/background.png"),
+                fit: BoxFit.cover),
+          ),
+          child: Center(
+            child: Text("Hello World"),
+          ),
+        ),
       ),
     );
   }
