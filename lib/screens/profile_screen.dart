c211c246253ce0336769db40de45c07fa5839c07
candra <candraherdiansyah14@gmail.com>
2022-11-08 16:11:14 +0700

    Profile Page 75%

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,45 +1,175 @@
 import 'package:flutter/material.dart';
-import 'package:tugas_mobile/helpers/size_helper.dart';
+import 'package:flutter/src/rendering/viewport.dart';
 
 class ProfileScreen extends StatelessWidget {
-  final double _top = 280 - 144;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
         height: double.infinity,
         width: double.infinity,
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           image: DecorationImage(
               image: AssetImage("assets/images/background.png"),
               fit: BoxFit.cover),
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Stack(
-              clipBehavior: Clip.none,
-              alignment: Alignment.center,
+            SizedBox(
+              height: 16,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CircleAvatar(
+              backgroundColor: Colors.black,
+              radius: 110,
+              child: CircleAvatar(
+                radius: 110,
+                backgroundImage: AssetImage('assets/images/profile.jpg'),
+              ),
+            ),
+            Container(
+              width: 360,
+              height: 50,
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.yellow.shade600,
+                      Colors.black26,
+                      Colors.blue.shade800
+                    ],
+                    begin: Alignment.bottomRight,
+                    end: Alignment.topLeft,
+                  ),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Center(
+                child: Text(
+                  "Candra Herdiansyah",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'DancingScript',
+                      fontSize: 24),
+                ),
+              ),
+            ),
+            Container(
+              width: 360,
+              height: 50,
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.yellow.shade600,
+                      Colors.black26,
+                      Colors.blue.shade800
+                    ],
+                    begin: Alignment.bottomRight,
+                    end: Alignment.topLeft,
+                  ),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Center(
+                child: Text(
+                  "Junior Developer",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'DancingScript',
+                      fontSize: 24),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Container(
-                  height: displayHeight(context) * 0.25,
-                  margin: EdgeInsets.all(16),
+                  width: 75,
+                  height: 75,
+                  margin: EdgeInsets.all(10),
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
                     image: DecorationImage(
-                        image: AssetImage("assets/images/logo.png"),
+                        image: AssetImage('assets/images/profile.jpg'),
                         fit: BoxFit.cover),
+                    gradient: LinearGradient(
+                        colors: [Colors.blueAccent, Colors.redAccent]),
+                    borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                Positioned(
-                  top: _top - 20,
-                  child: CircleAvatar(
-                    backgroundColor: Colors.blueGrey.shade700,
-                    radius: 144 / 3,
-                    backgroundImage: AssetImage("assets/images/profile.jpg"),
+                Container(
+                  width: 75,
+                  height: 75,
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/profile.jpg'),
+                        fit: BoxFit.cover),
+                    gradient: LinearGradient(
+                        colors: [Colors.blueAccent, Colors.redAccent]),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                Container(
+                  width: 75,
+                  height: 75,
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/profile.jpg'),
+                        fit: BoxFit.cover),
+                    gradient: LinearGradient(
+                        colors: [Colors.blueAccent, Colors.redAccent]),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                Container(
+                  width: 75,
+                  height: 75,
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/profile.jpg'),
+                        fit: BoxFit.cover),
+                    gradient: LinearGradient(
+                        colors: [Colors.blueAccent, Colors.redAccent]),
+                    borderRadius: BorderRadius.circular(10),
                   ),
                 ),
               ],
+            ),
+            Expanded(
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                children: [
+                  Container(
+                    width: 360,
+                    height: 360,
+                    padding:
+                        EdgeInsets.all(10), //You can use EdgeInsets like above
+                    margin: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Colors.yellow.shade700,
+                            Colors.black26,
+                            Colors.blue.shade800
+                          ],
+                          begin: Alignment.bottomRight,
+                          end: Alignment.topLeft,
+                        ),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'DancingScript',
+                          fontSize: 14),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
