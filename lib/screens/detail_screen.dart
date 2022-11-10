2025c1d10711268b5ea30fd73584c5a90e3e38a0
candra <candraherdiansyah14@gmail.com>
2022-11-10 14:58:21 +0700

    Appbar Detail

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -7,71 +7,78 @@ import 'package:flutter_svg/flutter_svg.dart';
 class DetailWisataScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("assets/images/background.png"),
-              fit: BoxFit.cover),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 32,
-            ),
-            Container(
-              alignment: Alignment.bottomLeft,
-              height: displayHeight(context) * 0.25,
-              margin: EdgeInsets.all(10),
-              padding: EdgeInsets.all(10), //You can use EdgeInsets like above
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(
-                    image: AssetImage("assets/images/ubud.jpg"),
-                    fit: BoxFit.cover),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Tour Detail"),
+        backgroundColor: Colors.blueGrey.shade900,
+      ),
+      body: SafeArea(
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/background.png"),
+                fit: BoxFit.cover),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 32,
               ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Expanded(
-              child: ListView(
-                shrinkWrap: true,
-                scrollDirection: Axis.vertical,
-                children: [
-                  Container(
-                    width: displayWidth(context) * 0.95,
-                    height: 360,
-                    padding:
-                        EdgeInsets.all(10), //You can use EdgeInsets like above
-                    margin: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Colors.yellow.shade700,
-                            Colors.black26,
-                            Colors.blue.shade800
-                          ],
-                          begin: Alignment.bottomRight,
-                          end: Alignment.topLeft,
-                        ),
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Text(
-                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                      textAlign: TextAlign.justify,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'DancingScript',
-                          fontSize: 14),
+              Container(
+                alignment: Alignment.bottomLeft,
+                height: displayHeight(context) * 0.25,
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(10), //You can use EdgeInsets like above
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/ubud.jpg"),
+                      fit: BoxFit.cover),
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Expanded(
+                child: ListView(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
+                  children: [
+                    Container(
+                      width: displayWidth(context) * 0.95,
+                      height: 360,
+                      padding: EdgeInsets.all(
+                          10), //You can use EdgeInsets like above
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.yellow.shade700,
+                              Colors.black26,
+                              Colors.blue.shade800
+                            ],
+                            begin: Alignment.bottomRight,
+                            end: Alignment.topLeft,
+                          ),
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Text(
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'DancingScript',
+                            fontSize: 14),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
