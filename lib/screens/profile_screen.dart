62aec373529aa0cfbf8006319c4bf1993448cb94
candra <candraherdiansyah14@gmail.com>
2022-11-17 16:10:06 +0700

    hyperlink

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -3,15 +3,26 @@ import 'package:flutter/src/rendering/viewport.dart';
 import 'package:tugas_mobile/helpers/size_helper.dart';
 import 'package:flutter_svg/avd.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final Uri _urlInstagram = Uri.parse('https://instagram.com/kangcandra_');
+  final Uri _urlFacebook = Uri.parse('https://facebook.com/kangcandraa');
+  final Uri _urlTwitter = Uri.parse('https://twitter.com/kangcandra_');
+  final Uri _urlLinkedin = Uri.parse('https://linkedin.com/in/kangcandraa');
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
         height: double.infinity,
         width: double.infinity,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           image: DecorationImage(
               image: AssetImage("assets/images/background.png"),
               fit: BoxFit.cover),
@@ -19,10 +30,10 @@ class ProfileScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(
+            const SizedBox(
               height: 32,
             ),
-            CircleAvatar(
+            const CircleAvatar(
               backgroundColor: Colors.black,
               radius: 110,
               child: CircleAvatar(
@@ -33,7 +44,7 @@ class ProfileScreen extends StatelessWidget {
             Container(
               width: displayWidth(context) * 0.95,
               height: 50,
-              margin: EdgeInsets.all(10),
+              margin: const EdgeInsets.all(10),
               decoration: BoxDecoration(
                   gradient: LinearGradient(
                     colors: [
@@ -45,7 +56,7 @@ class ProfileScreen extends StatelessWidget {
                     end: Alignment.topLeft,
                   ),
                   borderRadius: BorderRadius.circular(10)),
-              child: Center(
+              child: const Center(
                 child: Text(
                   "Candra Herdiansyah",
                   style: TextStyle(
@@ -58,7 +69,7 @@ class ProfileScreen extends StatelessWidget {
             Container(
               width: displayWidth(context) * 0.95,
               height: 50,
-              margin: EdgeInsets.symmetric(vertical: 10),
+              margin: const EdgeInsets.symmetric(vertical: 10),
               decoration: BoxDecoration(
                   gradient: LinearGradient(
                     colors: [
@@ -70,7 +81,7 @@ class ProfileScreen extends StatelessWidget {
                     end: Alignment.topLeft,
                   ),
                   borderRadius: BorderRadius.circular(10)),
-              child: Center(
+              child: const Center(
                 child: Text(
                   "Junior Developer",
                   style: TextStyle(
@@ -80,103 +91,123 @@ class ProfileScreen extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 16,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(
-                  width: 50,
-                  height: 50,
-                  margin: EdgeInsets.symmetric(horizontal: 10),
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Colors.transparent,
-                        Colors.transparent,
-                        Colors.transparent,
-                      ],
-                      begin: Alignment.bottomRight,
-                      end: Alignment.topLeft,
+                GestureDetector(
+                  onTap: () async {
+                    await openUrl(_urlTwitter.toString());
+                  },
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        colors: [
+                          Colors.transparent,
+                          Colors.transparent,
+                          Colors.transparent,
+                        ],
+                        begin: Alignment.bottomRight,
+                        end: Alignment.topLeft,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: SvgPicture.asset(
-                    "assets/svg_icons/twitter.svg",
-                    // color: Colors.blueGrey,
-                    matchTextDirection: true,
+                    child: SvgPicture.asset(
+                      "assets/svg_icons/twitter.svg",
+                      // color: Colors.blueGrey,
+                      matchTextDirection: true,
+                    ),
                   ),
                 ),
-                Container(
-                  width: 50,
-                  height: 50,
-                  margin: EdgeInsets.symmetric(horizontal: 10),
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Colors.transparent,
-                        Colors.transparent,
-                        Colors.transparent,
-                      ],
-                      begin: Alignment.bottomRight,
-                      end: Alignment.topLeft,
+                GestureDetector(
+                  onTap: () async {
+                    await openUrl(_urlFacebook.toString());
+                  },
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        colors: [
+                          Colors.transparent,
+                          Colors.transparent,
+                          Colors.transparent,
+                        ],
+                        begin: Alignment.bottomRight,
+                        end: Alignment.topLeft,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: SvgPicture.asset(
-                    "assets/svg_icons/facebook.svg",
-                    // color: Colors.blueGrey,
-                    matchTextDirection: true,
+                    child: SvgPicture.asset(
+                      "assets/svg_icons/facebook.svg",
+                      // color: Colors.blueGrey,
+                      matchTextDirection: true,
+                    ),
                   ),
                 ),
-                Container(
-                  width: 50,
-                  height: 50,
-                  margin: EdgeInsets.symmetric(horizontal: 10),
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Colors.transparent,
-                        Colors.transparent,
-                        Colors.transparent,
-                      ],
-                      begin: Alignment.bottomRight,
-                      end: Alignment.topLeft,
+                GestureDetector(
+                  onTap: () async {
+                    await openUrl(_urlInstagram.toString());
+                  },
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        colors: [
+                          Colors.transparent,
+                          Colors.transparent,
+                          Colors.transparent,
+                        ],
+                        begin: Alignment.bottomRight,
+                        end: Alignment.topLeft,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: SvgPicture.asset(
-                    "assets/svg_icons/instagram.svg",
-                    // color: Colors.blueGrey,
-                    matchTextDirection: true,
+                    child: SvgPicture.asset(
+                      "assets/svg_icons/instagram.svg",
+                      // color: Colors.blueGrey,
+                      matchTextDirection: true,
+                    ),
                   ),
                 ),
-                Container(
-                  width: 50,
-                  height: 50,
-                  margin: EdgeInsets.symmetric(horizontal: 10),
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Colors.transparent,
-                        Colors.transparent,
-                        Colors.transparent,
-                      ],
-                      begin: Alignment.bottomRight,
-                      end: Alignment.topLeft,
+                GestureDetector(
+                  onTap: () async {
+                    await openUrl(_urlLinkedin.toString());
+                  },
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        colors: [
+                          Colors.transparent,
+                          Colors.transparent,
+                          Colors.transparent,
+                        ],
+                        begin: Alignment.bottomRight,
+                        end: Alignment.topLeft,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: SvgPicture.asset(
-                    "assets/svg_icons/linkedin.svg",
-                    // color: Colors.blueGrey,
-                    matchTextDirection: true,
+                    child: SvgPicture.asset(
+                      "assets/svg_icons/linkedin.svg",
+                      // color: Colors.blueGrey,
+                      matchTextDirection: true,
+                    ),
                   ),
                 ),
               ],
             ),
-            SizedBox(
+            const SizedBox(
               height: 16,
             ),
             Expanded(
@@ -187,9 +218,9 @@ class ProfileScreen extends StatelessWidget {
                   Container(
                     width: displayWidth(context) * 0.95,
                     height: 360,
-                    padding:
-                        EdgeInsets.all(10), //You can use EdgeInsets like above
-                    margin: EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(
+                        10), //You can use EdgeInsets like above
+                    margin: const EdgeInsets.all(10),
                     decoration: BoxDecoration(
                         gradient: LinearGradient(
                           colors: [
@@ -201,7 +232,7 @@ class ProfileScreen extends StatelessWidget {
                           end: Alignment.topLeft,
                         ),
                         borderRadius: BorderRadius.circular(10)),
-                    child: Text(
+                    child: const Text(
                       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                       textAlign: TextAlign.justify,
                       style: TextStyle(
@@ -217,5 +248,31 @@ class ProfileScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> openUrl(
+    String url, {
+    forceSafariVC = false,
+    forceWebView = false,
+    enableJavaScript = true,
+  }) async {
+    try {
+      await launchUrl(Uri.parse(url), mode: LaunchMode.externalApplication);
+    } catch (e) {
+      print(e);
+      // Get.snackbar(
+      //   'Error',
+      //   'Tidak bisa membuka link',
+      //   icon: const Icon(Icons.error),
+      //   backgroundColor: Colors.red,
+      //   colorText: Colors.white,
+      //   forwardAnimationCurve: Curves.bounceIn,
+      //   margin: const EdgeInsets.only(
+      //     top: 10,
+      //     left: 5,
+      //     right: 5,
+      //   ),
+      // );
+    }
   }
 }
